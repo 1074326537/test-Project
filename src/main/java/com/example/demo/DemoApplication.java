c0b803ba6266630e8f95a1e89ec82fcef01a31fc
gaopeng <1074326537@qq.com>
2021-07-08 08:35:19 +0800

    测试本地提交git

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,9 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        //Second_branch  版本提交
+
     }
 
 }
